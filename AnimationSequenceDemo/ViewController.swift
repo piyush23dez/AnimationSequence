51acf52306e405dbb30ddbfcfe051215f3a2ee1f
Piyush Sharma <piyush_sharma2@apple.com>
2017-01-27 21:29:29 -0800

    Initial Commit

---
+++ AnimationSequenceDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AnimationSequenceDemo
+//
+//  Created by Piyush Sharma on 1/27/17.
+//  Copyright © 2017 Piyush Sharma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
